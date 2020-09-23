4a502ff1de74dffdbde0d3e0d88fe1c2078bf02f
Francesco Giarola <francesco.giarola@elfo.net>
2020-09-23 22:54:03 +0200

    startup + pokemon data

---
+++ pokemon-a-la-shakespeare/Controllers/PokemonController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokeApiNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pokemon_a_la_shakespeare.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        private readonly PokeApiClient pokeApiClient;
+
+        public PokemonController(PokeApiClient pokeApiClient)
+        {
+            this.pokeApiClient = pokeApiClient;
+        }
+
+        [HttpGet]
+        public string Get() => $"Try with a pokemon name such as pikachu!!";
+
+        [HttpGet("{pokemonName}")]
+        public async Task<string> Get(string pokemonName)
+        {
+            var pokemonSpecies = await pokeApiClient.GetResourceAsync<PokemonSpecies>(pokemonName);
+
+            return $"Hello {pokemonSpecies.Name}, specie: {pokemonSpecies.FlavorTextEntries.First(x => x.Language.Name == "en").FlavorText}!!";
+        }
+    }
+}
