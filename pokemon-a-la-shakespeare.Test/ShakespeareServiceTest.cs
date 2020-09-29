98d9e9a16158ba31be3beab66973659e75d1fd0f
Francesco Giarola <francesco.giarola@elfo.net>
2020-09-29 22:44:10 +0200

    Shakespeare Service Test

---
+++ pokemon-a-la-shakespeare.Test/ShakespeareServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pokemon_a_la_shakespeare.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokemon_a_la_shakespeare.Test
+{
+    [TestClass]
+    public class ShakespeareServiceTest
+    {
+        private ShakespeareService shakespeareService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.shakespeareService = new ShakespeareService();
+        }
+
+        [TestMethod]
+        public async Task Poetyze_FromNormalToShakespeareText()
+        {
+            var normalText = "You gave Mr. Tim a hearty meal, but unfortunately what he ate made him die.";
+            var expectedShakespeareText = "Thee did giveth mr. Tim a hearty meal,  but unfortunately what he did doth englut did maketh him kicketh the bucket.";
+            var shakespeareText = await this.shakespeareService.PoetyzeAsync(normalText);
+
+            Assert.AreEqual(expectedShakespeareText, shakespeareText);
+        }
+    }
+}
